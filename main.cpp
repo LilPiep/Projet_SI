8e4286a571304fe49905d367a57651dbb9b183a1
Pierrick Catteau <pierrick.catteau@etu.univ-grenoble-alpes.fr>
2023-03-27 00:03:55 +0200

    about textures

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "include/glad/glad.h"
 #include <math.h>
+#include "include/stb/stb_image.h"
 
 #include "include/VBO.h"
 #include "include/VAO.h"
@@ -13,19 +14,16 @@
 
 // Verices coordinates
     GLfloat vertices[] = {
-        // Positions          // Colors
-        -0.5f,  -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f, 0.0f,
-        0.5f,   -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.6f, 0.1f,
-        0.0f,   0.5f * float(sqrt(3)) * 2 / 3,  0.0f,   1.0f, 0.5f, 0.3f,
-        -0.25f, 0.5f * float(sqrt(3)) / 6,      0.0f,   0.7f, 0.5f, 0.3f,
-        0.25f,  0.5f * float(sqrt(3)) / 6,      0.0f,   0.8f, 0.4f, 0.15f,
-        0.0f,   -0.5f * float(sqrt(3)) / 3,     0.0f,   0.9f, 0.3f, 0.2f
+        // Positions                 // Colors
+        -0.5f,  -0.5f,    0.0f,   1.0f, 0.0f, 0.0f,     0.0f, 0.0f, 
+        -0.5f,   0.5f,    0.0f,   0.0f, 1.0f, 0.0f,     0.0f, 1.0f, 
+        0.5f,    0.5f,    0.0f,   0.0f, 0.0f, 1.0f,     1.0f, 1.0f, 
+        0.5f,    -0.5f,   0.0f,   1.0f, 1.0f, 1.0f,     1.0f, 0.0f 
     };
 
     GLuint indices[] = {
-        0, 3, 5,
-        3, 2, 4,
-        5, 4, 1
+        0, 2, 1,
+        0, 3, 2
     };
 
 int main(){
@@ -68,14 +66,45 @@ int main(){
     EBO EBO1(indices, sizeof(indices));
 
     // Links VBO to VAO
-    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     // Unbiinding all the buffers
     VAO1.Unbind();
     VBO1.Unbind();
     EBO1.Unbind();
 
     GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    // Texture
+
+    int widthImg, heightImg, numColCh;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char* bytes = stbi_load("shrek.png", &widthImg, &heightImg, &numColCh, 0);
+
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // float flatColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
+    // glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, flatColor);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
 
     // Main loop
     while (!glfwWindowShouldClose(window)) 
@@ -87,8 +116,9 @@ int main(){
         // Tell OpenGL to use the shader program
         shaderProgram.Activate();
         glUniform1f(uniID, 0.5f); // Very important to type this AFTER shaderProgram.Activate()
+        glBindTexture(GL_TEXTURE_2D, texture);
         VAO1.Bind();
-        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
 
         glfwPollEvents();
@@ -97,6 +127,7 @@ int main(){
     VAO1.Delete();
     VBO1.Delete();
     EBO1.Delete();
+    glDeleteTextures(1, &texture);
     shaderProgram.Delete();
 
     glfwDestroyWindow(window);
