7b511e99d3b9a297a1aad415d1023ce1ad8d4dde
Pierrick Catteau <pierrick.catteau@etu.univ-grenoble-alpes.fr>
2023-03-26 10:47:25 +0200

    quelques ajustements + manipulations

--- main.cpp
+++ main.cpp
@@ -1,12 +1,40 @@
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+#include "include/glad/glad.h"
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(a.Pos.x, a.Pos.y, a.Pos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(0,8f, 0.3f, 0,2f, 1.0f);\n"
+"}\n\0";
+
 
 int main(){
+
+    // Initialize GLFW
     glfwInit();
+
+    // Set the version of OpenGL we want to use = 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // profile = package of functions, here we choose core profile since we want to work with modern functions
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f * 0.0f,
+        0.5f, -0.5f *  3, 0.0f,
+        0.0f, 0.5f *  3, 0.0f
+    };
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "Volcano Project", NULL, NULL);
     
@@ -15,9 +43,31 @@ int main(){
         printf("Failed to create GLFW window");
         glfwTerminate();
         return -1;
-    }
+    } 
 
     glfwMakeContextCurrent(window);
+
+    //Init glad
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 800); // The area of the window we want OpenGl to render in from (0,0) to (800,800)
+
+    // Create a vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
 
     while (!glfwWindowShouldClose(window))
     {
